75cdf8f07adef444ba19be3a35afec2b60c3e40d
SavkovAndrey <141626360+SavkovAndrey@users.noreply.github.com>
2023-11-07 12:52:23 +0300

    Создание конструкторов

--- Hard_09/prism.cpp
+++ Hard_09/prism.cpp
@@ -2,3 +2,46 @@
 #include "prism.h"
 using namespace std;
 
+//-------------------------  ŒÕ—“–” “Œ– œŒ ”ÃŒÀ◊¿Õ»ﬁ
+
+Prism::Prism()
+{
+	num_of_edge = 4;
+	long_of_side = 1;
+	height = 1;
+	area = (num_of_edge * long_of_side * long_of_side) / (4 * tan((180 / num_of_edge) * PI / 180));
+	volume = area * height;
+}
+
+//-------------------------  ŒÕ—“–” “Œ– — œ¿–¿Ã≈“–¿Ã»
+
+Prism::Prism(int n, double a, double h) : num_of_edge(n), long_of_side(a), height(h)
+{
+	area = (num_of_edge * long_of_side * long_of_side) / (4 * tan((180 / num_of_edge) * PI / 180));
+	volume = area * height;
+}
+
+
+//-------------------------  ŒÕ—“–” “Œ–  Œœ»–Œ¬¿Õ»ﬂ
+
+Prism::Prism(Prism& other)
+{
+	this->num_of_edge = other.num_of_edge;
+	this->long_of_side = other.long_of_side;
+	this->height = other.height;
+	this->area = other.area;
+	this->volume = other.volume;
+}
+
+//------------------------- ƒ≈—“–” “Œ–
+
+Prism::~Prism()
+{
+
+}
+
+//------------------------- √≈““≈–€
+
+
+//------------------------- —≈““≈–€
+
--- Hard_09/prism.h
+++ Hard_09/prism.h
@@ -5,7 +5,23 @@ using namespace std;
 
 class Prism
 {
+private:
 
+	double  volume;                            // объем 
+	int    height;                             // высота
+	int    num_of_edge;                        // число граней
+	double  long_of_side;                      // длинна сторон
+	double area;                               // площадь основания
+	const double PI = 3.141592653589793;       // Пи
+public:
+
+	Prism();                                   // КОНСТРУКТОР по умолчанию
+	Prism(int n, double a, double h);          // КОНСТРУКТОР с параметрами
+	Prism(Prism& other);                       // КОНСТРУКТОР копирования 
+	~Prism();                                  // ДЕСТРУКТОР
+	
+
+	
 
 
 };
